b9c7d3927d68dc979762f7cab5b11a8e0f5cf1dd
Swaroop <swaroop.atp@gmail.com>
2025-06-19 00:49:06 +0530

    added special variables

--- install-command.sh
+++ install-command.sh
@@ -7,15 +7,21 @@ then
     echo "ERROR:: You cant  install without root user"
     exit 1 #give other than 0 upto 127
 else
-    echo "Approved"
+    echo "Approved:: You are running with root access"
 fi
 
-dnf install mysql -y
-
-if [ $? -eq 0 ]
+dnf list installed mysql
+if [ $? -ne 0 ]
 then
-    echo "Installing Mysql is SUCCESS"
-else
-    echo "Installing Mysql is FAILED"
+    echo "Mysql is not installed in server. Going to install it..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing Mysql is SUCCESS"
+    else
+        echo "Installing Mysql is FAILED"
     exit 1
+    fi
+else
+    echo "Mysql is already installed in server"
 fi
