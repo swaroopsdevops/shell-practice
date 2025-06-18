8ad5519bdedfd1c373247dddb25e9c5389b79f40
Swaroop <swaroop.atp@gmail.com>
2025-06-19 00:32:23 +0530

    added special variables

--- install-command.sh
+++ install-command.sh
@@ -5,9 +5,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You cant  install without root user"
-    exit 1 #give other than 0
+    exit 1 #give other than 0 upto 127
 else
     echo "Approved"
 fi
 
-dnf install mysql -y
+dnf install gfgfgggfgg -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installing Mysql is SUCCESS"
+else
+    echo "Installing Mysql is FAILED"
+    #exit 1
+fi
