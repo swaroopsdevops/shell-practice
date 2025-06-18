7d349372944bae874550a23774ad54bc365ae076
Swaroop <swaroop.atp@gmail.com>
2025-06-18 23:37:24 +0530

    added special variables

--- special-vars.sh
+++ special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variable: $#"
+echo "Current Script name: $0"
+echo "Current directory: $PWD"
+echo "Current User Home Directory: $HOME"
+echo "User running the script: $USER"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
