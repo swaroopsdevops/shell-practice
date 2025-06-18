56fce0f1cc24049120a0dfef181210e4419e14da
Swaroop <swaroop.atp@gmail.com>
2025-06-19 00:24:01 +0530

    added special variables

--- install-command.sh
+++ install-command.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You cant  install without root user"
+    exit 1 #give other than 0
 else
     echo "Approved"
 fi
--- special-vars.sh
+++ special-vars.sh
@@ -7,5 +7,6 @@ echo "Current directory: $PWD"
 echo "Current User Home Directory: $HOME"
 echo "User running the script: $USER"
 echo "PID of the script: $$"
+echo "exit status: $?"
 sleep 10 &
 echo "PID of the last command in background: $!"
