a7dc983c6f6e99858019e279e3d84880c845fe7b
Swaroop <swaroop.atp@gmail.com>
2025-06-19 23:31:02 +0530

    colors

--- loops.sh
+++ loops.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER=/var/log/shellscript-logs
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "nginx" "python" "httpd")
+PACKAGES=("mysql" "nginx" "python3" "httpd")
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" tee -a $LOG_FILE
