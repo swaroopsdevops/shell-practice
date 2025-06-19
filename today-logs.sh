d2de3cd0a96db1b40156bee66151948a2b0721ce
Swaroop <swaroop.atp@gmail.com>
2025-06-19 22:42:31 +0530

    colors

---
+++ today-logs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/log/shellscript-logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILE
+else
+    echo -e "$G APPROVED: You are running this script with root access $N"
+fi
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+        echo -e "Installing $2 is ... $G SUCCESS $N"
+    else
+        echo -e "Installing $2 is ... $R FAILED $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo -e "$G mysql is not installed. lets do it. $N" &>>$LOG_FILE
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e "$Y mysql is already installed. $N" &>>$LOG_FILE
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$G python3 is not installed. lets do it. $N" &>>$LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+else
+    echo -e "$Y python3 is already installed.$N" &>>$LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$G nginx is not installed. lets do it. $N" &>>$LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "$Y nginx is already installed. $N" &>>$LOG_FILE
+fi
